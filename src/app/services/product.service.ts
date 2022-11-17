e9f083da437cd96dc3e90ea600551fa52a1bcf2b
Monika017017 <m.piatek017@gmail.com>
2022-11-17 22:54:15 +0100

    Categories checkbox

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,7 +8,7 @@ export class ProductService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAll(): Observable<ProductModel[]> {
-    return this._httpClient.get<ProductModel[]>('https://fakestoreapi.com/products');
+  create(product: Omit<ProductModel, 'id'>): Observable<ProductModel> {
+    return this._httpClient.post<ProductModel>('https://fakestoreapi.com/products', product);
   }
 }
