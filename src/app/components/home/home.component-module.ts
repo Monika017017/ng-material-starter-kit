01423567c4d0af24fd6d0b1d030720144c341a25
Monika017017 <m.piatek017@gmail.com>
2022-11-23 19:00:51 +0100

    Routing and details

--- src/app/components/home/home.component-module.ts
+++ src/app/components/home/home.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import {RouterLinkWithHref} from "@angular/router";
 
 @NgModule({
-  imports: [RouterLinkWithHref, RouterLinkWithHref],
+  imports: [RouterLinkWithHref, ReactiveFormsModule, RouterLinkWithHref],
   declarations: [HomeComponent],
   providers: [],
   exports: [HomeComponent]
