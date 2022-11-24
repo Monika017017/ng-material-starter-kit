ae44853b1de4e979307d5142f2ccebd2e901c7f7
Monika017017 <m.piatek017@gmail.com>
2022-11-24 18:35:26 +0100

    Routing and details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {EmployeeListComponent} from './components/employee-list/employee-list.component';
-import {EmployeeFormComponent} from './components/employee-form/employee-form.component';
-import {HomeComponent} from './components/home/home.component';
-import {ProductTableComponent} from './components/product-table/product-table.component';
-import {EmployeeDetailsComponent} from './components/employee-details/employee-details.component';
-import {EmployeeListComponentModule} from './components/employee-list/employee-list.component-module';
-import {EmployeeListServiceModule} from './services/employee-list.service-module';
-import {EmployeeFormComponentModule} from './components/employee-form/employee-form.component-module';
-import {EmployeeServiceModule} from './services/employee.service-module';
-import {HomeComponentModule} from './components/home/home.component-module';
-import {ProductTableComponentModule} from './components/product-table/product-table.component-module';
-import {ProductTableServiceModule} from './services/product-table.service-module';
-import {EmployeeDetailsComponentModule} from './components/employee-details/employee-details.component-module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-form.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductTableComponent } from './components/product-table/product-table.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { EmployeeListComponentModule } from './components/employee-list/employee-list.component-module';
+import { EmployeeListServiceModule } from './services/employee-list.service-module';
+import { EmployeeFormComponentModule } from './components/employee-form/employee-form.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
+import { HomeComponentModule } from './components/home/home.component-module';
+import { ProductTableComponentModule } from './components/product-table/product-table.component-module';
+import { ProductTableServiceModule } from './services/product-table.service-module';
+import { EmployeeDetailsComponentModule } from './components/employee-details/employee-details.component-module';
 
-const routes: Routes = [{path: 'employee-list', component: EmployeeListComponent}, {
-  path: 'create-employee',
-  component: EmployeeFormComponent
-}, {path: '', component: HomeComponent}, {
-  path: 'product-table',
-  component: ProductTableComponent
-}, {path: 'product-search', component: ProductTableComponent}, {
-  path: 'employee-details/:id/:name',
-  component: EmployeeDetailsComponent
-}];
+const routes: Routes = [{ path: 'employee-list', component: EmployeeListComponent }, { path: 'create-employee', component: EmployeeFormComponent }, { path: '', component: HomeComponent }, { path: 'product-table', component: ProductTableComponent }, { path: 'product-search', component: ProductTableComponent }, { path: 'employee-details/:id/:name', component: EmployeeDetailsComponent }, { path: 'employee-form', component: EmployeeFormComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeListServiceModule, EmployeeFormComponentModule, EmployeeServiceModule, HomeComponentModule, ProductTableComponentModule, ProductTableServiceModule, EmployeeDetailsComponentModule],
